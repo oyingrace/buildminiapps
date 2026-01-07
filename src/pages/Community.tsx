909801772ca36380e6709ac2aaba27a4d5f475b8
oyingrace <oyingrace307@gmail.com>
2026-01-07 15:32:18 +0100

    feat: initialize Community component with state and navigation hooks

--- src/pages/Community.tsx
+++ src/pages/Community.tsx
@@ -5,3 +5,8 @@ import { Link, useNavigate } from 'react-router-dom';
 import Footer from '../components/Footer';
 import api from '@/configs/axios';
 import { toast } from 'sonner';
+
+const Community = () => {
+    const [loading, setLoading] = useState(true);
+    const [projects, setProjects] = useState<Project[]>([])
+    const navigate = useNavigate()
