9e7901d90aee0a70c380a880aef430dc8538f4fe
oyingrace <oyingrace307@gmail.com>
2026-01-07 15:32:58 +0100

    feat: Add `fetchProjects` function to `Community` component for fetching published projects.

--- src/pages/Community.tsx
+++ src/pages/Community.tsx
@@ -10,3 +10,14 @@ const Community = () => {
     const [loading, setLoading] = useState(true);
     const [projects, setProjects] = useState<Project[]>([])
     const navigate = useNavigate()
+
+    const fetchProjects = async () => {
+        try {
+            const { data } = await api.get('/api/project/published');
+            setProjects(data.projects);
+            setLoading(false);
+        } catch (error: any) {
+            console.log(error);
+            toast.error(error?.response?.data?.message || error.message);
+        }
+    }
