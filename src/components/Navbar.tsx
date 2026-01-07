743374d278f306acd73f61467d2d0c75cce8dd3d
oyingrace <oyingrace307@gmail.com>
2026-01-07 09:12:16 +0100

    feat: Add Navbar component with menu and credit state, session data, and credit fetching functionality.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,3 +5,20 @@ import { authClient } from '@/lib/auth-client';
 import { UserButton } from '@daveyplate/better-auth-ui'
 import api from '@/configs/axios';
 import { toast } from 'sonner';
+
+const Navbar = () => {
+    const [menuOpen, setMenuOpen] = React.useState(false);
+    const navigate = useNavigate()
+    const [credits, setCredits] = useState(0)
+
+    const { data: session } = authClient.useSession()
+
+    const getCredits = async () => {
+        try {
+            const { data } = await api.get('/api/user/credits');
+            setCredits(data.credits)
+        } catch (error: any) {
+            toast.error(error?.response?.data?.message || error.message)
+            console.log(error);
+        }
+    }
