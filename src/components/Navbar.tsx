77fc41cdaceba12b7de21871aa7b62ceb7dc9f7e
oyingrace <oyingrace307@gmail.com>
2026-01-07 09:11:41 +0100

    feat: add Navbar component with essential module imports

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,7 @@
+import React, { useEffect, useState } from 'react'
+import { assets } from '../assets/assets';
+import { Link, useNavigate } from 'react-router-dom';
+import { authClient } from '@/lib/auth-client';
+import { UserButton } from '@daveyplate/better-auth-ui'
+import api from '@/configs/axios';
+import { toast } from 'sonner';
