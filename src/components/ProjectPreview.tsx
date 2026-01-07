119a7c33a287638da6a4df97f1e38d6afd26f740
oyingrace <oyingrace307@gmail.com>
2026-01-07 09:37:32 +0100

    feat: add ProjectPreview component

---
+++ src/components/ProjectPreview.tsx
@@ -0,0 +1,5 @@
+import React, { forwardRef, useEffect, useImperativeHandle, useRef, useState } from 'react'
+import type { Project } from '../types';
+import { iframeScript } from '../assets/assets';
+import EditorPanel from './EditorPanel';
+import LoaderSteps from './LoaderSteps';
