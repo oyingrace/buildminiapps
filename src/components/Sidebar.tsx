fe48992517b5ee8354c442a4e26fe9568f29f391
oyingrace <oyingrace307@gmail.com>
2026-01-07 09:45:23 +0100

    feat: add `SidebarProps` interface to define component props.

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,3 +4,11 @@ import { BotIcon, EyeIcon, Loader2Icon, SendIcon, UserIcon } from 'lucide-react'
 import { Link } from 'react-router-dom';
 import api from '@/configs/axios';
 import { toast } from 'sonner';
+
+interface SidebarProps {
+    isMenuOpen: boolean;
+    project: Project,
+    setProject: (project: Project) => void;
+    isGenerating: boolean;
+    setIsGenerating: (isGenerating: boolean) => void;
+}
