a7f05d2928ac7b9082c9bff37ea106674cb37aaa
oyingrace <oyingrace307@gmail.com>
2026-01-07 08:41:09 +0100

    refactor: Add EditorPanelProps interface to define component properties.

--- src/components/EditorPanel.tsx
+++ src/components/EditorPanel.tsx
@@ -1,3 +1,19 @@
 import { X } from 'lucide-react';
 import React, { useEffect, useState } from 'react'
 
+interface EditorPanelProps {
+    selectedElement: {
+        tagName: string;
+        className: string;
+        text: string;
+        styles: {
+            padding: string;
+            margin: string;
+            backgroundColor: string;
+            color: string;
+            fontSize: string;
+        };
+    } | null;
+    onUpdate: (updates: any) => void;
+    onClose: () => void;
+}
