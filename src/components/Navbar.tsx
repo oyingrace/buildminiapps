5d1f19d655c62bd37e5a84e6595076eea657ca3c
oyingrace <oyingrace307@gmail.com>
2026-01-07 09:15:20 +0100

    feat: add mobile menu toggle button to Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -59,3 +59,11 @@ const Navbar = () => {
 
                     )
                     }
+
+                    <button id="open-menu" className="md:hidden active:scale-90 transition" onClick={() => setMenuOpen(true)} >
+                        <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M4 5h16" /><path d="M4 12h16" /><path d="M4 19h16" /></svg>
+                    </button>
+                </div>
+
+
+            </nav>
