ff7d6a0b187f1323ca1f83c14457d0620c45f4b5
oyingrace <oyingrace307@gmail.com>
2026-01-08 09:13:38 +0100

    fix: Implement error handling with toast notifications and reset loading state for preview data.

--- src/pages/Preview.tsx
+++ src/pages/Preview.tsx
@@ -25,3 +25,9 @@ const Preview = () => {
                     }
                 })
             }
+            setLoading(false)
+        } catch (error: any) {
+            toast.error(error?.response?.data?.message || error.message);
+            console.log(error);
+        }
+    }
