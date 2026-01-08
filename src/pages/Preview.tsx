cfb3b2279487dadb538433a0ad1dad1ddc586c8f
oyingrace <oyingrace307@gmail.com>
2026-01-08 09:11:18 +0100

    feat: Add imports for project and version types, API client, toast notifications, and authentication client.

--- src/pages/Preview.tsx
+++ src/pages/Preview.tsx
@@ -2,3 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Loader2Icon } from "lucide-react";
 import ProjectPreview from "../components/ProjectPreview";
+import type { Project, Version } from "../types";
+import api from "@/configs/axios";
+import { toast } from "sonner";
+import { authClient } from "@/lib/auth-client";
