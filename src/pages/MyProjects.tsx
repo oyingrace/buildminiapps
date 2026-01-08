86fd8eab1d6b00bfec61180e4ef279abf8234ec7
oyingrace <oyingrace307@gmail.com>
2026-01-08 09:03:13 +0100

    feat: Add loading spinner and responsive padding to the MyProjects page.

--- src/pages/MyProjects.tsx
+++ src/pages/MyProjects.tsx
@@ -46,3 +46,10 @@ const MyProjects = () => {
             toast('Please login to view your projects');
         }
     }, [session?.user])
+    return (
+    <>
+            <div className='px-4 md:px-16 lg:px-24 xl:px-32'>
+                {loading ? (
+                    <div className='flex items-center justify-center h-[80vh]'>
+                        <Loader2Icon className='size-7 animate-spin text-indigo-200' />
+                    </div>
