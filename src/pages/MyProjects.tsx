676d54934f7be12f7d25d56d193ce80797335b09
oyingrace <oyingrace307@gmail.com>
2026-01-08 09:08:36 +0100

    feat: Add Footer component to the MyProjects page.

--- src/pages/MyProjects.tsx
+++ src/pages/MyProjects.tsx
@@ -47,7 +47,7 @@ const MyProjects = () => {
         }
     }, [session?.user])
     return (
-    <>
+        <>
             <div className='px-4 md:px-16 lg:px-24 xl:px-32'>
                 {loading ? (
                     <div className='flex items-center justify-center h-[80vh]'>
@@ -114,3 +114,9 @@ const MyProjects = () => {
                     </div>
                 )}
             </div>
+            <Footer />
+        </>
+    )
+}
+
+export default MyProjects
