2f3056384147d6e0e7aafd8b8feb86cf859f009d
oyingrace <oyingrace307@gmail.com>
2026-01-08 08:51:17 +0100

    feat: render a spinning loader icon on the Loading page

--- src/pages/Loading.tsx
+++ src/pages/Loading.tsx
@@ -8,3 +8,12 @@ const Loading = () => {
             window.location.href = '/'
         }, 6000)
     }, [])
+
+    return (
+        <div className='h-screen flex flex-col'>
+            <div className='flex items-center justify-center flex-1'>
+                <Loader2Icon className='size-7 animate-spin text-indigo-200' />
+            </div>
+        </div>
+    )
+}
