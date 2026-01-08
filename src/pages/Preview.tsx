9554b7ad11cac957e4ae987f351e3a59385cc2bc
oyingrace <oyingrace307@gmail.com>
2026-01-08 09:10:59 +0100

    feat: add Preview page component.

---
+++ src/pages/Preview.tsx
@@ -0,0 +1,4 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Loader2Icon } from "lucide-react";
+import ProjectPreview from "../components/ProjectPreview";
