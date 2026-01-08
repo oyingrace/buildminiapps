d68060034a4fcee983264c444b46f87e7eba5c7b
oyingrace <oyingrace307@gmail.com>
2026-01-08 08:50:48 +0100

    feat: Add Loading component that redirects to the home page after 6 seconds.

--- src/pages/Loading.tsx
+++ src/pages/Loading.tsx
@@ -1,2 +1,10 @@
 import { Loader2Icon } from 'lucide-react'
 import { useEffect } from 'react'
+
+const Loading = () => {
+
+    useEffect(() => {
+        setTimeout(() => {
+            window.location.href = '/'
+        }, 6000)
+    }, [])
