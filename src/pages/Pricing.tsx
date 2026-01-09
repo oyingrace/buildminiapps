3b6198fdb6a494bc996876a44d4d982419a801e8
oyingrace <oyingrace307@gmail.com>
2026-01-09 16:53:50 +0100

    feat: Initialize session data and pricing plans state in the Pricing page.

--- src/pages/Pricing.tsx
+++ src/pages/Pricing.tsx
@@ -29,3 +29,11 @@ const Pricing = () => {
             console.log(error);
         }
     }
+
+    return (
+    <>
+            <div className='w-full max-w-5xl mx-auto z-20 max-md:px-4 min-h-[80vh]'>
+                <div className='text-center mt-16'>
+                    <h2 className='text-gray-100 text-3xl font-medium'>Choose Your Plan</h2>
+                    <p className='text-gray-400 text-sm max-w-md mx-auto mt-2'>Start for free and scale up as you grow. Find the perfect plan for your content creation needs.</p>
+                </div>
