b539bd126625ab28781eae8d3af6ca4d7b85b6b3
oyingrace <oyingrace307@gmail.com>
2026-01-09 16:45:55 +0100

    feat: Add React Router's BrowserRouter and global Providers to the application entry point.

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,5 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom'
+import { Providers } from "./providers.tsx"
