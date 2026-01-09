aa5a898f0de659f8758a4f340a31a8e58a5d4727
oyingrace <oyingrace307@gmail.com>
2026-01-09 16:41:48 +0100

    define navigate const

--- src/providers.tsx
+++ src/providers.tsx
@@ -1,3 +1,6 @@
 import { AuthUIProvider } from "@daveyplate/better-auth-ui"
 import { authClient } from "@/lib/auth-client"
 import { useNavigate, NavLink } from "react-router-dom"
+
+export function Providers({ children }: { children: React.ReactNode }) {
+    const navigate = useNavigate()
