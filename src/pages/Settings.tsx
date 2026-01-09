e95bc06210f6e25ed7653113c052d2b23c5a1cd1
oyingrace <oyingrace307@gmail.com>
2026-01-09 17:49:15 +0100

    style: Apply custom styling and layout to the AccountSettingsCards component.

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -2,3 +2,12 @@ import { AccountSettingsCards, ChangePasswordCard, DeleteAccountCard } from "@da
 
 const Settings = () => {
     return (
+
+        <div className="w-full p-4 flex justify-center items-center min-h-[90vh] flex-col gap-6 py-12">
+            <AccountSettingsCards
+                classNames={{
+                    card: {
+                        base: 'bg-black/10 ring ring-indigo-950 max-w-xl mx-auto',
+                        footer: 'bg-black/10 ring ring-indigo-950'
+                    }
+                }} />
