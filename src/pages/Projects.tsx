98f79c65e7aaacf07938f4ef2b13919f8f11ba23
oyingrace <oyingrace307@gmail.com>
2026-01-09 17:02:25 +0100

    feat: Add project preview, sidebar, and various icons to the Projects page.

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,3 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { Link, useNavigate, useParams } from 'react-router-dom'
 import type { Project } from '../types'
+import { ArrowBigDownDashIcon, EyeIcon, EyeOffIcon, FullscreenIcon, LaptopIcon, Loader2Icon, MessageSquareIcon, SaveIcon, SmartphoneIcon, TabletIcon, XIcon } from 'lucide-react'
+import Sidebar from '../components/Sidebar'
+import ProjectPreview, { type ProjectPreviewRef } from '../components/ProjectPreview'
