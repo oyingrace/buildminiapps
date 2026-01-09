b027430e8fdef2dbf704356fba732c6f12fc4529
oyingrace <oyingrace307@gmail.com>
2026-01-09 17:51:46 +0100

    feat: Add ChangePasswordCard component to Settings page

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -11,3 +11,9 @@ const Settings = () => {
                         footer: 'bg-black/10 ring ring-indigo-950'
                     }
                 }} />
+            <div className="w-full">
+                <ChangePasswordCard classNames={{
+                    base: 'bg-black/10 ring ring-indigo-950 max-w-xl mx-auto',
+                    footer: 'bg-black/10 ring ring-indigo-950'
+                }} />
+            </div>
