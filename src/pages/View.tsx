e82df00443ffa696b100eae4a66cda80fe30f735
oyingrace <oyingrace307@gmail.com>
2026-01-09 17:56:42 +0100

    feat: Enhance View component by importing dummyProjects and Loader2Icon

--- src/pages/View.tsx
+++ src/pages/View.tsx
@@ -1,2 +1,4 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
+import { dummyProjects } from "../assets/assets";
+import { Loader2Icon } from "lucide-react";
