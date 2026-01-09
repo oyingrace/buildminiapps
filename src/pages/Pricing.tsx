33bf6ee1ba2d05663b96221beddf1bebd7bb1858
oyingrace <oyingrace307@gmail.com>
2026-01-09 16:51:18 +0100

    define plan interface properties

--- src/pages/Pricing.tsx
+++ src/pages/Pricing.tsx
@@ -4,3 +4,12 @@ import Footer from '../components/Footer';
 import { authClient } from '@/lib/auth-client';
 import { toast } from 'sonner';
 import api from '@/configs/axios';
+
+interface Plan {
+    id: string;
+    name: string;
+    price: string;
+    credits: number;
+    description: string;
+    features: string[];
+}
