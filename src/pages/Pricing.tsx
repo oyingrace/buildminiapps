6ea0e25cc4a3d3acd0b49c515f6a3f759d9b4d07
oyingrace <oyingrace307@gmail.com>
2026-01-09 16:50:51 +0100

    feat: Add initial Pricing page component.

---
+++ src/pages/Pricing.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { appPlans } from '../assets/assets';
+import Footer from '../components/Footer';
+import { authClient } from '@/lib/auth-client';
+import { toast } from 'sonner';
+import api from '@/configs/axios';
