cf9210c564560f5d4bc6199eab970cd33bc61bbf
oyingrace <oyingrace307@gmail.com>
2026-01-09 16:47:41 +0100

    feat: Render the App component within BrowserRouter and Providers for routing and context management.

--- src/main.tsx
+++ src/main.tsx
@@ -3,3 +3,11 @@ import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import { Providers } from "./providers.tsx"
+
+createRoot(document.getElementById('root')!).render(
+    <BrowserRouter>
+        <Providers>
+            <App />
+        </Providers>
+    </BrowserRouter>,
+)
